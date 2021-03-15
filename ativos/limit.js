fd111e10737c549297b0c43860cea21503da3052
leoapk <80705605+leoapk@users.noreply.github.com>
2021-03-15 14:40:29 -0400

    Add files via upload

---
+++ ativos/limit.js
@@ -0,0 +1,24 @@
+const limit = (prefix, botName, ownerName) => {
+        return `
+「 *${botName}* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: 🔥☠︎︎𝗥⃟𝗡𝗧☠︎︎͜͡🇧🇷🔥
+  ❏ Version: 0.0.4
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *LIMIT*
+  │
+  ├─ ❏ ${prefix}limit
+  ├─ ❏ ${prefix}bal
+  └─ ❏ ${prefix}buylimit
+--------------------------------
+Note: Kamu bisa membeli limit dengan cara mengumpulkan uang terlebih dahulu. Cek uang kamu dengan cara mengetikan perintah *${prefix}bal* , harga 1 limit = 1000 uang.`
+}
+exports.limit = limit
